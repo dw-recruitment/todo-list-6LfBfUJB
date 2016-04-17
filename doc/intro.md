dd3cf8b4dec9d4993a33b737e981e4607fc1b475
Anonymous Candidate <anon@example.com>
2016-04-17 17:54:34 -0600

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tv-todo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/tv_todo/core.clj
@@ -0,0 +1,7 @@
+(ns tv-todo.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
