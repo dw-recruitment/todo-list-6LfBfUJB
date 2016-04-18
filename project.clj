a6c936e3721958428a6bfbc6020c324cd39c3dbd
Anonymous Candidate <anon@example.com>
2016-04-17 19:42:26 -0600

    Complete Step 4

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]]
   :main ^:skip-aot tv-todo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
