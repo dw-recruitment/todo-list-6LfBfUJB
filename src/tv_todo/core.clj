e2edc3e5cfcaf3d9274fd50b105b956513498e13
Anonymous Candidate <anon@example.com>
2016-04-17 18:10:33 -0600

    Complete Step 2

--- src/tv_todo/core.clj
+++ src/tv_todo/core.clj
@@ -1,4 +1,16 @@
 (ns tv-todo.core
-  (:gen-class))
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jet]))
 
-(defn -main)
+(defn under-construction []
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<h1>Todos List</h1>"
+              "<hr>"
+              "<img src='http://www.animatedimages.org/data/media/1432/animated-pikachu-image-0018.gif' style='height: 400px;'>")})
+
+(defn handler [request]
+  (case (:uri request)
+    "/" (under-construction)))
+
+(defn -main [] (jet/run-jetty handler {:port 8000}))
