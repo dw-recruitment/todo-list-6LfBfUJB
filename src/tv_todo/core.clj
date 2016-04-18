884ea48d803d11e422ffd66836bdba48a7abf2fa
Anonymous Candidate <anon@example.com>
2016-04-17 18:19:16 -0600

    make a layout that accepts views

--- src/tv_todo/core.clj
+++ src/tv_todo/core.clj
@@ -2,15 +2,18 @@
   (:gen-class)
   (:require [ring.adapter.jetty :as jet]))
 
-(defn under-construction []
+(defn layout [view]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (str "<h1>Todos List</h1>"
               "<hr>"
-              "<img src='http://www.animatedimages.org/data/media/1432/animated-pikachu-image-0018.gif' style='height: 400px;'>")})
+              view)})
+
+(def under-construction
+  "<img src='http://www.animatedimages.org/data/media/1432/animated-pikachu-image-0018.gif' style='height: 400px;'>")
 
 (defn handler [request]
   (case (:uri request)
-    "/" (under-construction)))
+    "/" (layout under-construction)))
 
 (defn -main [] (jet/run-jetty handler {:port 8000}))
